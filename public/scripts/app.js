0b2ee91d776bf3f0b04337819cfde8193179ae23
Alejandro Rodarte <arodarte@keyinnovation.org>
2019-10-08 17:53:11 -0600

    Section 8: Hello React.

---
+++ public/scripts/app.js
@@ -0,0 +1,12 @@
+// JSX: JavaScript XML -> JavaScript syntax extension
+// const template = <p>This is JSX from app.js!</p>;
+
+const template = React.createElement("h1", {
+    id: "some-id"
+}, "Something new!");
+
+const appRoot = document.getElementById('app');
+
+// render template with ReactDOM
+// two arguments: the JSX template itself and where will it be located (app root div)
+ReactDOM.render(template, appRoot);
