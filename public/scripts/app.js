92d44ef7062b29c987fd7fbaefb379a15a74f1f9
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-01-02 11:17:12 -0700

    10. Exploring JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,13 +2,56 @@
 
 // JSX: JavaScript XML -> JavaScript syntax extension
 var template = React.createElement(
-  'h1',
-  null,
-  'Does this change?'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Does this change?'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
+);
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Alejandro Rodarte'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 24'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: Ciudad Juarez'
+    )
 );
 
 var appRoot = document.getElementById('app');
 
 // render template with the ReactDOM
 // two arguments: the JSX template itself and where will it be located (app root div)
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,8 +1,43 @@
 // JSX: JavaScript XML -> JavaScript syntax extension
-const template = <h1>Does this change?</h1>;
+const template = (
+    <div>
+        <h1>
+            Does this change?
+        </h1> 
+        
+        <p>
+            This is some info
+        </p>
+
+        <ol>
+            <li>
+                Item one
+            </li>
+            <li>
+                Item two
+            </li>
+        </ol>
+    </div>
+);
+
+const templateTwo = (
+    <div>
+        <h1>
+            Alejandro Rodarte
+        </h1>
+
+        <p>
+            Age: 24
+        </p>
+
+        <p>
+            Location: Ciudad Juarez
+        </p>
+    </div>
+);
 
 const appRoot = document.getElementById('app');
 
 // render template with the ReactDOM
 // two arguments: the JSX template itself and where will it be located (app root div)
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
