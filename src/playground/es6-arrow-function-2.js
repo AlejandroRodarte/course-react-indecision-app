13c28b2b9bfd41303f71eda4a87eda13d809528a
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-01-03 16:33:14 -0700

    15. ES6 Aside: Arrow Functions Part II

--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -1,14 +1,13 @@
-'use strict';
-
 // arguments object - no longer bound with arrow functions
 
-var add = function add(a, b) {
+const add = (a, b) => {
 
     // the 'arguments' object provides all the arguments injected when a function is called
     // this object is no longer accessible on arrow functions
     // console.log(arguments);
 
     return a + b;
+
 };
 
 console.log(add(2, 3));
@@ -17,7 +16,7 @@ console.log(add(2, 3));
 
 // when we declare an ES5 function inside an object, the `this` keyword is bound to that object
 // an arrow function will NOT have bound the object `this` keyword
-var user = {
+const user = {
     name: 'Alejandro',
     cities: ['Juarez', 'Chihuahua', 'Tijuana'],
 
@@ -26,15 +25,11 @@ var user = {
 
     // making it an arrow function would attempt to get the `this` value from the parent scope, which in this case would be
     // the Window object
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
+    printPlacesLived() {
         // the arrow function declared in the map method does not bound a `this` keyword, so we can STILL access this.name;
         // if the function passed in is a regular ES5 function, a `this` would be attempted to be bound, and being anonymous
         // would make `this` to be undefined
-        return this.cities.map(function (city) {
-            return _this.name + ' has lived in ' + city;
-        });
+        return this.cities.map(city => `${this.name} has lived in ${city}`);
     }
 };
 
@@ -42,15 +37,11 @@ console.log(user.printPlacesLived());
 
 // challenge area
 
-var multiplier = {
+const multiplier = {
     numbers: [7, 2, 9],
     multiplyBy: 5,
-    multiply: function multiply() {
-        var _this2 = this;
-
-        return this.numbers.map(function (num) {
-            return num * _this2.multiplyBy;
-        });
+    multiply() {
+        return this.numbers.map(num => num * this.multiplyBy);
     }
 };
 
