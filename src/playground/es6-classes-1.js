366ba7202e854db7601cacf2f9e1f6596729bf62
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-01-05 21:23:09 -0700

    25. ES6 Classes: Part II

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -10,13 +10,63 @@ class Person {
     }
 
     getDescription() {
-        return `${this.name} is ${this.age} year(s) old`
+        return `${this.name} is ${this.age} year(s) old.`
     }
 
 }
 
-const me = new Person('Alejandro Rodarte', 24);
-console.log(me.getDescription());
+class Student extends Person {
 
-const other = new Person();
-console.log(other.getDescription());
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+
+    hasMajor() {
+        return !!this.major;
+    }
+
+    getDescription() {
+
+        let description = super.getDescription();
+
+        if (this.hasMajor()) {
+            description += ` Their major is ${this.major}.`
+        }
+
+        return description;
+
+    }
+
+}
+
+class Traveler extends Person {
+
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+
+    hasHomeLocation() {
+        return !!this.homeLocation;
+    }
+
+    getGreeting() {
+
+        let greeting = super.getGreeting();
+
+        if (this.hasHomeLocation()) {
+            greeting += ` I'm visiting from ${this.homeLocation}.`
+        }
+
+        return greeting;
+
+    }
+
+}
+
+const me = new Traveler('Alejandro Rodarte', 24, 'Ciudad Juarez');
+console.log(me.getGreeting());
+
+const other = new Traveler();
+console.log(other.getGreeting());
