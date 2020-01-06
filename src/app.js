37a29c0d933e69ddc923203b88645f64da90b645
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-01-06 11:08:10 -0700

    41. Default Prop Values

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ class IndecisionApp extends React.Component {
         super(props);
 
         this.state = {
-            options: []
+            options: props.options
         }
 
         this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
@@ -49,14 +49,13 @@ class IndecisionApp extends React.Component {
 
     render() {
 
-        const title = 'Indecision';
         const subtitle = 'Put your life in the hands of a computer';
 
         // for children to manipulate parent data (upstream flow) pass down functions as regular props
         return (
             <div>
 
-                <Header title={ title } subtitle={ subtitle } />
+                <Header subtitle={ subtitle } />
 
                 <Action 
                     hasOptions={ this.state.options.length > 0 }
@@ -79,18 +78,24 @@ class IndecisionApp extends React.Component {
 
 }
 
+IndecisionApp.defaultProps = {
+    options: []
+};
+
 const Header = (props) => {
     return (
         <div>
             <h1>
                 { props.title }
             </h1>
-            <h2>
-                { props.subtitle }
-            </h2>
+            { props.subtitle && <h2> { props.subtitle } </h2> }
         </div>
     );
 }
+
+Header.defaultProps = {
+    title: 'Indecision'
+};
 
 const Action = (props) => {
     return (
@@ -192,4 +197,4 @@ class AddOption extends React.Component {
 //     );
 // };
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp options={ ['Option one', 'Option two'] }/>, document.getElementById('app'));
--- src/playground/counter-example-state.js
+++ src/playground/counter-example-state.js
@@ -9,7 +9,7 @@ class Counter extends React.Component {
         this.handleReset = this.handleReset.bind(this);
 
         this.state = {
-            count: 0
+            count: props.count
         };
 
     }
@@ -35,7 +35,7 @@ class Counter extends React.Component {
     handleReset() {
         this.setState((prevState) => {
             return {
-                count: 0
+                count: this.props.count
             };
         });
     }
@@ -64,4 +64,8 @@ class Counter extends React.Component {
 
 }
 
-ReactDOM.render(<Counter />, document.getElementById('app'));
+Counter.defaultProps = {
+    count: 0
+};
+
+ReactDOM.render(<Counter count={ -10 } />, document.getElementById('app'));
