ff3efd7b27a447b588156d3df96581551af60f05
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-01-06 22:54:00 -0700

    52. Default Exports

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 // named import
-import { square, add } from './utils';
+import subtract, { square, add } from './utils';
 
-import { isAdult, canDrink } from './person';
+import isSenior, { isAdult, canDrink } from './person';
 
 console.log('app.js is running');
 console.log(square(5));
 console.log(add(3, 8));
+console.log(subtract(5, 2));
 
 console.log(isAdult(17));
 console.log(isAdult(19));
 
 console.log(canDrink(20));
 console.log(canDrink(22));
+
+console.log(isSenior(64));
+console.log(isSenior(66));
--- src/utils.js
+++ src/utils.js
@@ -4,5 +4,9 @@ const square = x => x * x;
 
 const add = (a, b) => a + b;
 
+// const subtract = (a, b) => a - b;
+
+export default (a, b) => a - b;
+
 // named exports: export by using the exact variable name
 export { square, add };
