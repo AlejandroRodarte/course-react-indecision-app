ea70a6a59e6b14658002130c26d0805a094ecb8f
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-01-06 22:42:43 -0700

    51. ES6 import/export

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
+// named import
+import { square, add } from './utils';
+
+import { isAdult, canDrink } from './person';
+
 console.log('app.js is running');
+console.log(square(5));
+console.log(add(3, 8));
+
+console.log(isAdult(17));
+console.log(isAdult(19));
+
+console.log(canDrink(20));
+console.log(canDrink(22));
---
+++ src/utils.js
@@ -0,0 +1,8 @@
+console.log('utils.js is running');
+
+const square = x => x * x;
+
+const add = (a, b) => a + b;
+
+// named exports: export by using the exact variable name
+export { square, add };
